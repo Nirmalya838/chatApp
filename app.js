d2e30e7aeb69b828a976ebc4d2d69a44e06a5d4d
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-07-10 01:31:34 +0530

    CORS handled

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")))
+
+app.use(cors({origin: 'http://127.0.0.1:3000',
+methods: ['GET', 'POST']}));
 
 const sequelize = require('./util/database');
 const signupRouter = require('./routes/signupRoute');
